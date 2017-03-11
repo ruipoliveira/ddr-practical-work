903e32d7e7b7c21e1126bfdd40684e77ed175d13
Guilherme Cardoso <gjc@ua.pt>
2017-03-11 19:32:06 +0000

    melhorada a main da 1º simulacao

--- guide1/simulation_first.m
+++ guide1/simulation_first.m
@@ -1,5 +1,5 @@
 %Parameters initialization:
-N= 50; % Number of mobile nodes
+N= 100; % Number of mobile nodes
 W= 40; % Radio range (in meters)
 S= 15; % Maximum speed (in Km/h)
 delta= 1; % Difference between consecutive time instants (in seconds)
@@ -19,6 +19,7 @@ axis([0 300 0 200])
 grid on
 % Simulation cycle running all time instants iter:
 for iter= 1:T
+  figure(1)
   hold off
   % Visualize updated node positions:
   plot(pos(:,1),pos(:,2),'o','MarkerEdgeColor','b','MarkerFaceColor','b')
@@ -31,15 +32,17 @@ for iter= 1:T
   results(iter)= AverageConnectedNodePairs(N,L);
   % Update node coordinates and speed values:
   [pos,vel]= UpdateCoordinates(pos,vel,delta);
-  pause(0.01)
+  pause(0.001)
 end
 
+hold off
 % Plot in a different window the simulation results
 average = zeros(1,T);
 for i=1:T
     average(1,i) = mean(results(1:i));
 end
 figure(2)
+axis([0 T 0 1])
 plot((1:T)',results',(1:T)',average');
 
 % Compute the final result:
