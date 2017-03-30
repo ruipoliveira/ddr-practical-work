c559358662f67ce40b74d0e311b0a3c32d61fc99
Guilherme Cardoso <gjc@ua.pt>
2017-03-30 15:02:40 +0100

    l

--- guide2/task2.m
+++ guide2/task2.m
@@ -22,10 +22,36 @@ for i=1:size(pAll,2)
     results(i,2) = (pEF2*pF2) / (pEF1*pF1 + pEF2*pF2);
 end
 results
-results = [];
 
 % 2.b. For each value of p and for n = 2, 3, 4 and 5, determine the probability of false positives
-% and fulfil the follow table: 
+% and fulfil the follow table:
+
+results = [];
+for n=2:5
+    for i=1:size(pAll,2)
+        pEF1 = 0.0001^n;
+        pEF2 = 0.5^n;
+        pF1 = pAll(i);
+        pF2 = 1 - pAll(i);
+        
+        results(i,n-1) = (pEF1*pF1) / (pEF1*pF1 + pEF2*pF2);
+    end
+end
+results
+results = [];
 
 
+results = [];
+for n=2:5
+    for i=1:size(pAll,2)
+        pEF1 = 1- 0.0001^n;
+        pEF2 = 1- 0.5^n;
+        pF1 = pAll(i);
+        pF2 = 1 - pAll(i);
+        
+        results(i,n-1) = (pEF2*pF2) / (pEF1*pF1 + pEF2*pF2);
+    end
+end
+results
+results = [];
 
