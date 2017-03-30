db8794393e469b54c14f577455222ec1b6a926cd
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-03-30 13:18:49 +0100

    update ex b

--- guide2/task1.m
+++ guide2/task1.m
@@ -6,10 +6,8 @@ brv = binornd(n,q)
 
 
 % geometric random variable
-p = 0.01;
-grv = geornd(0.01)
 
-
+grv = geornd(0.01);
 
 % 1a) Determine the probability of a data packet of total size B = 100 bytes
 % being received without errors, with exactly 1 error and with 2 or more
@@ -33,14 +31,33 @@ results(3,:) = 1 - results(1,:) - results(2,:);
 
 results
 results = [];
-% 2b)
+
+% 1b) Consider that 10% of data packets has a total size B=64 bytes and 90% of data packets has a 
+%total size B=1500 bytes. Determine the packet discard rate (i.e., the percentage of packets that are discarded) and fulfil the following table:
 
 for j=1:3
     results(j) = 1;
     for k=0:1
-        results(j) = results(j) - (0.90 * binopdf(k, 1500*8, error_rates(j))) + (0.10 * binopdf(k, 64*8, error_rates(j)));
+        results(j) = (0.90 * (results(j) - binopdf(k, 1500*8, error_rates(j)))) + (0.10 * (results(j) -binopdf(k, 64*8, error_rates(j))));
     end
 end
 
-
 results
+results = [];
+
+% 1c) Consider now that the total size, in number of Bytes, of the data packets is a constant value (64 Bytes) plus a geometric random value with parameter p=0.02. Determine the
+%packet discard rate on this case and fulfil the following table:
+
+p = 0.02;
+for i=0:1000
+for j=1:3
+    results(j) = 0;	
+	n = (64 +i)* 8;
+    geo_value = p*(1-p).^i; 
+    results(j) = results(j) + (1- binopdf(0,n, error_rates(j))) +(1- binopdf(1,n, error_rates(j))) * geo_value;
+end
+
+end 
+results
+
+
