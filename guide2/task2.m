836d282a3e041b5b62e554491a9369d9306f3871
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-03-30 14:21:21 +0100

    up

--- guide2/task2.m
+++ guide2/task2.m
@@ -1,3 +1,15 @@
-%2a)
+%2a) For each value of p, determine the probability of the link being in the interference state
+%and in the normal state when one control frame is received with errors (fulfil the
+%following table). What do you conclude?
+
+pAll = [99, 99.9, 99.99, 99.999]; 
+
+pEF1 = 0;
+pEF2 = 0;
+pF1 = 0; 
+pF2 = 0; 
+
+
+pF1E = (pEF1*pF1) / (pEF1*pF1 + pEF2*pF2)
 
 
