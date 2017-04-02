980311c1b171a522b7eb1d96c41cc334bd2aad9b
Guilherme Cardoso <gjc@ua.pt>
2017-04-02 22:19:57 +0100

    l

--- guide2/task1.m
+++ guide2/task1.m
@@ -2,7 +2,7 @@
 
 n = 1;      % nr de ensaios
 q = 10^-7;  % probabilidade de sucesso
-brv = binornd(n,q)
+brv = binornd(n,q);
 
 
 % geometric random variable
@@ -13,13 +13,14 @@ grv = geornd(0.01);
 % being received without errors, with exactly 1 error and with 2 or more
 % errors. Fulfil the following table:
 
+% determinar probabilidade
+
 p = [0 1 2];    % probability of 0, 1 and 2 errors
-B = 800;        % packet size of 100 bytes
+B = 800;        % packet size of 100 bytes (100*8 bits)
 
 error_rates = [ 10^-7 10^-6 10^-5];
 
 results = [];
-format long
 
 for i=0:1
     for j=1:3
@@ -29,6 +30,7 @@ end
 
 results(3,:) = 1 - results(1,:) - results(2,:);
 
+format SHORTE;
 results
 results = [];
 
@@ -36,13 +38,18 @@ results = [];
 %90% of data packets has a total size B=1500 bytes. Determine the packet
 %discard rate (i.e., the percentage of packets that are discarded) and fulfil the following table:
 
+% determinar rate (percentagem)
+format short; 
+
 for j=1:3
     results(j) = 1;
     for k=0:1
         results(j) = (0.90 * (results(j) - binopdf(k, 1500*8, error_rates(j)))) + (0.10 * (results(j) -binopdf(k, 64*8, error_rates(j))));
     end
+    results(j) = results(j) * 100; % convert to %
 end
 
+format short;
 results
 results = [];
 
@@ -50,6 +57,8 @@ results = [];
 %packets is a constant value (64 Bytes) plus a geometric random value with
 %parameter p=0.02. Determine the packet discard rate on this case and
 %fulfil the following table:
+
+% determinar rate (percentagem)
 
 p = 0.02;
 
@@ -60,7 +69,10 @@ for j=1:3
         geo_value = p*(1-p).^i;
         results(j) = results(j) + (1- binopdf(0,n, error_rates(j)) - binopdf(1,n, error_rates(j))) * geo_value;
     end
+    results(j) = results(j) * 100; % convert to %
 end
+
+format short;
 results
 results=[];
 
@@ -70,10 +82,14 @@ results=[];
 % on each data packet which only enables the receiver to detect if the data packet has been
 % received with errors. Repeat 1.b. and 1.c. for this case and fulfill the following tables:
 
+% ambos em percentagem
+
 for j=1:3
     results(j) = (0.90 * (1- binopdf(0, 1500*8 -28*8, error_rates(j)))) + (0.10 * (1- binopdf(0, 64*8 - 28*8, error_rates(j))));
+    results(j) = results(j) * 100;
 end
 fprintf('1d, rep 1b')
+format short;
 results
 results = [];
 
@@ -86,7 +102,9 @@ for j=1:3
         geo_value = p*(1-p).^i;
         results(j) = results(j) +  (1-binopdf(0,n, error_rates(j))) * geo_value;
     end
+    results(j) = results(j) * 100;
 end
 fprintf('1d, rep 1c')
+format short;
 results
-results=[];
+
