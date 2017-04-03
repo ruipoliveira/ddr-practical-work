e30abfd1203e9b1a8ed7d0c0377bbf194fdc6ed6
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-04-03 10:02:45 +0100

    up

--- guide2/task3.m
+++ guide2/task3.m
@@ -1,10 +1,16 @@
+format long; 
 
-% implementation markov
+states = [0 1 2 3 4];
+bers = [ 10^-5 10^-4 10^-3 10^-2 10^-1];
+br = [1 5 5 10]; % birthrate
+dr = [100 50 50 20]; % deathrate
 
+%old implementation
 br0 = 1; 
 br1 = 5;
 br2 = 5; 
-br3 = 10;  
+br3 = 10;
+
 ded1 = 100; 
 ded2 = 50; 
 ded3 = 50; 
@@ -12,6 +18,12 @@ ded4 = 20;
 
 fprintf('task 3a \n');
 
+for i=1:size(states,2)
+   st = states(i);
+   fprintf('%d : %d \n',st,markov_func(st,br,dr)*100); 
+end
+
+%old implementation
 P0 =  1 / (1 + (br0/ded1) + ((br0*br1)/(ded1*ded2)) + ((br0*br1*br2)/(ded1*ded2*ded3)) + ((br0*br1*br2*br3)/(ded1*ded2*ded3*ded4)));
 P0_per = P0*100
 
@@ -27,13 +39,21 @@ P3_per = P3*100
 P4 = ((br0*br1*br2*br3)/(ded1*ded2*ded3*ded4))* P0;
 P4_per = P4*100
 
-
 fprintf('task 3b \n');
 
+biterror=0;
+for i=1:size(states,2)
+   st = states(i);
+   biterror = biterror + markov_func(st,br,dr)*bers(i);
+end
+
+biterror
+
+%old implementation
 biterror = P0 * 10^-5 + P1 * 10^-4 + P2 * 10^-3 + P3 * 10^-2 + P4 * 10^-1 
 
 fprintf('task 3c \n');
-
+	
 duration0 = (1/br0)*60
 duration1 = (1/(br1+ded1))*60
 duration2 = (1/(br2+ded2))*60
