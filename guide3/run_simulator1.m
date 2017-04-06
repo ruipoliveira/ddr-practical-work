4f2a944aff3139651a50142bd34d79947ff4091d
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-04-06 14:56:33 +0100

    com intervalo de confianca

--- guide3/run_simulator1.m
+++ guide3/run_simulator1.m
@@ -6,15 +6,13 @@ minutes_all = [90 95 90 95 90 95 90 95 90 95 90 95];
 
 R = 10000; 
 
-N = 20;
+N = 10;
 
 result_b = zeros(1,N);
 result_o = zeros(1,N);
 
-
 format short; 
 for it= 1:N
-
 	for i= 1:size(C_all,2)
 		[b o] = simulator1(lambda_all(i), minutes_all(i), C_all(i), M ,R ); 
 		result_b(i) = b*100; 
@@ -26,10 +24,15 @@ end
 alfa= 0.1; %intervalo de confiança a 90%
 media = mean(result_b);
 termo = norminv(1-alfa/2)*sqrt(var(result_b)/N);
-fprintf('resultado = %.2e +- %.2e\n',media,termo)
+fprintf('resultado_b = %.2e +- %.2e\n',media,termo)
+fprintf('resultado_b = %.2e // %.2e\n',media+termo, media-termo)
 
 
-
+alfa= 0.1; %intervalo de confiança a 90%
+media = mean(result_o);
+termo = norminv(1-alfa/2)*sqrt(var(result_o)/N);
+fprintf('resultado_o = %.2e +- %.2e\n',media,termo)
+fprintf('resultado_o = %.2e // %.2e\n',media+termo, media-termo)
 
 
 
