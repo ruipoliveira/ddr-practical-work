b5a20a8324d4c3770915cd49ab8462c46f986eec
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-04-20 14:42:35 +0100

    ds

--- guide3/simulator2.m
+++ guide3/simulator2.m
@@ -17,13 +17,12 @@ function [b_s b_h]= simulator2(lambda,p,invmiu,S, W, Ms, Mh, R, N)
 	ARRIVAL_S = 0; 
 	ARRIVAL_H = 0; 
 
-	DEPARTURE_S = zeros(1,S);
-	DEPARTURE_H = zeros(1,S);
+	DEPARTURE_S = ones(1,S);
+	DEPARTURE_H = ones(1,S);
 	
 	%State variables initialization:
 	STATE= zeros(1,S);
-	STATE_S = 100; 
-
+	STATE_S = 0;
 
 	%Statistical counters initialization:
 	LOAD= 0;
@@ -34,35 +33,58 @@ function [b_s b_h]= simulator2(lambda,p,invmiu,S, W, Ms, Mh, R, N)
 	BLOCKED_H= 0;
 	BLOCKED_S = 0; 
 
-	
 	%Simulation Clock and initial List of Events:
 	Clock= 0;
-	EventList= [ARRIVAL exprnd(invlambda)];
-	
+	EventList= [ARRIVAL_S exprnd(invlambda); ARRIVAL_H exprnd(invlambda)];
+	EventList= sortrows(EventList,2);
+
+
+	NARRIVALS = ARRIVAL_S + ARRIVAL_S; 
+
+
 	while NARRIVALS < R
 		event= EventList(1,1);
-		Previous_Clock= Clock;
 		Clock= EventList(1,2);
+		server_id = EventList(1,3); 
+		Previous_Clock= Clock;
+
 		EventList(1,:)= [];
 
 		LOAD= LOAD + STATE*(Clock-Previous_Clock);
 		
-		if event == ARRIVAL
+
+		% in standard
+		if event == ARRIVAL_S
+			EventList= [EventList; ARRIVAL Clock+exprnd(invlambda) 0];
+			NARRIVALS= NARRIVALS+1;
+
+			min_state_id = find(STATE==min(STATE))
+
+			if STATE + M <= C % verificar condicoes pdf 
+				STATE= STATE+M;
+				EventList= [EventList; DEPARTURE Clock+exprnd(invmiu) server_id];
+			else
+				BLOCKED= BLOCKED+1;
+			end 
+			
+		% in high-definition
+		elseif event == ARRIVAL_H
 			EventList= [EventList; ARRIVAL Clock+exprnd(invlambda)];
 			NARRIVALS= NARRIVALS+1;
-			if STATE + M <= C
+			if STATE + M <= C % verificar 
 				STATE= STATE+M;
 				EventList= [EventList; DEPARTURE Clock+exprnd(invmiu)];
 			else
 				BLOCKED= BLOCKED+1;
 			end
+
 		else
-			STATE= STATE-M;
+			if STATE== DEPARTURE_H 
 		end
 		EventList= sortrows(EventList,2);
+
 	end
 	
-	b= BLOCKED/NARRIVALS;
-	o= LOAD/Clock;
-
+	b_s = BLOCKED_S/NARRIVALS_S;
+	b_h = BLOCKED_H/NARRIVALS_H;
 end
