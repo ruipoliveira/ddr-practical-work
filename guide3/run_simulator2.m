abf4caf8b0aff7496b179453c719f240cf4c4ae9
Guilherme Cardoso <gjc@ua.pt>
2017-04-30 19:32:42 +0100

    update

--- guide3/run_simulator2.m
+++ guide3/run_simulator2.m
@@ -53,7 +53,7 @@ N = 1000;       % one month warm up
 b_s = zeros(1,100);
 b_h = zeros(1,100);
 
-runs = 40;
+runs = 5;
 W_limit = 100;
 
 b_s_confidence = zeros(W_limit,2);
@@ -69,8 +69,8 @@ for W=1:W_limit
     fprintf('W:%.0f : %.6f +- %.6f || %.6f +- %.6f\n', W, b_s_confidence(W,1)*100, b_s_confidence(W,2)*100, b_h_confidence(W,1)*100, b_h_confidence(W,2)*100);
 end
 
-
-plot(1:W_limit,b_s_confidence, 1:W_limit, b_h_confidence);
+k = 5
+plot(1:W_limit,b_s_confidence(:,1), 1:W_limit, b_h_confidence(:,1));
 
 end
 
@@ -88,25 +88,39 @@ N = 10000;
 S_limit = 50;
 W_limit = 50;
 
-b_s = zeros(W_limit, S_limit);
-b_h = zeros(W_limit, S_limit);
+runs = 40;
+
+b_s_confidence = zeros(W_limit, S_limit);
+b_s_confidence_error = zeros(W_limit, S_limit);
+b_h_confidence = zeros(W_limit, S_limit);
+b_h_confidence_error = zeros(W_limit, S_limit);
 
 for S=15:S_limit
     for W=0:W_limit
-        [b_s(W+1,S), b_h(W+1,S)] = simulator2(lambda, p, invmiu, S, W, Ms, Mh, R, N);
-        fprintf('W %.0f S %.0f: %.5f || %.5f\n', W, S, b_s(W+1,S), b_h(W+1,S))
+        
+        b_s = zeros(runs);
+        b_h = zeros(runs);
+        
+        for lap=1:runs
+            [b_s(lap), b_h(lap)] = simulator2(lambda, p, invmiu, S, W, Ms, Mh, R, N);
+        end
+        
+        [b_s_confidence(W+1,S), b_s_confidence_error(W+1,S)] = confidence_level(0.1, b_s, runs);
+        [b_h_confidence(W+1,S), b_h_confidence_error(W+1,S)] = confidence_level(0.1, b_h, runs);
+        
+        fprintf('W %.0f S %.0f: %.5f || %.5f\n', W, S, b_s_confidence(W+1,S), b_h_confidence(W+1,S))
     end
 end
 
 % compute worse case of the two streams
-worse_case = zeros(size(b_s,1), size(b_s,2));
+worse_case = zeros(size(b_s_confidence,1), size(b_s_confidence,2));
 
-for i=1:size(b_s,1)
-    for j=1:size(b_s,2)
-        if b_s(i,j) >= b_h(i,j)
-            worse_case(i,j) = b_s(i,j);
+for i=1:size(b_s_confidence,1)
+    for j=1:size(b_s_confidence,2)
+        if b_s_confidence(i,j) >= b_h_confidence(i,j)
+            worse_case(i,j) = b_s_confidence(i,j);
         else
-            worse_case(i,j) = b_h(i,j);
+            worse_case(i,j) = b_h_confidence(i,j);
         end
     end
 end
@@ -122,7 +136,7 @@ grid on
 % a = [2,2,3;0,2,5;1 2 3]
 % [row,column]=find(a==min(min(a(a>0))))
 
-[S_optimal, W_optimal] = find(worse_case==min(min(worse_case(worse_case>0))))
+[W_optimal, S_optimal] = find(worse_case==min(min(worse_case(worse_case>0))))
 
 w=1
 end
