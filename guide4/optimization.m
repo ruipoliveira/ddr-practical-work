f9bf77e7bfb1b7d67eac5ba089b1cca2f81d1f33
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-05-25 13:54:12 +0100

    init function

--- guide4/optimization.m
+++ guide4/optimization.m
@@ -1,3 +1,42 @@
+
+GlobalBest= Inf;
+for iter=1:Iterations
+	CurrentSolution= GreedyRandomized();
+	CurrentObjective= Evaluate(CurrentSolution);
+	repeat= true;
+	while repeat
+		NeighbourBest= Inf;
+		for i=1:size(CurrentSolution,1)
+			NeighbourSolution= BuildNeighbour(CurrentSolution,i);
+			NeighbourObjective= Evaluate(NeighbourSolution);
+			if NeighbourObjective < NeighbourBest
+				NeighbourBest= NeighbourObjective;
+				NeighbourBestSolution= NeighbourSolution;
+			end
+		end
+		if NeighbourBest < CurrentObjective
+			CurrentObjective= NeighbourBest;
+			CurrentSolution= NeighbourBestSolution;
+		else
+			repeat= false;
+		end
+	end
+	if CurrentObjective < GlobalBest
+		GlobalBestSolution= CurrentSolution;
+		GlobalBest= CurrentObjective;
+	end
+end
+
+
+
+
+
+
+
+
+
+
+%%%%%% 
 pairs
 npairs = size(pairs, 1); 
 b = randperm(npairs)
