5f2d23d9320c102d9f467498d47cefd3d10aa5c8
Rui Oliveira <ruipedrooliveira@ua.pt>
2017-06-01 14:40:36 +0100

    f

--- guide4/BuildNeighbour.m
+++ guide4/BuildNeighbour.m
@@ -1,4 +1,6 @@
 function res = BuildNeighbour(solution, it)
+	
+	
 
-
+	
 end
--- guide4/ex_e_optimization.m
+++ guide4/ex_e_optimization.m
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:guide4/ex_e_optimization.m
 Matrizes;
 miu= R*1e9/(8*1000);            % capacidade em bits / pacotes de 1000bytes -> pacotes/sec
 NumberLinks= sum(sum(R>0));     % numero total de ligacoes
@@ -22,8 +23,12 @@ routes= zeros(npairs,17);
 % a.routes
 % a.lambda
 
+=======
+>>>>>>> Stashed changes:guide4/optimization.m
 GlobalBest= Inf;
+Iterations = 2; 
 for iter=1:Iterations
+<<<<<<< Updated upstream:guide4/ex_e_optimization.m
     CurrentSolution= GreedyRandomized();
     CurrentObjective= Evaluate(CurrentSolution);
     repeat= true;
@@ -48,7 +53,38 @@ for iter=1:Iterations
         GlobalBestSolution= CurrentSolution;
         GlobalBest= CurrentObjective;
     end
+=======
+	CurrentSolution= GreedyRandomized();
+	CurrentObjective= Evaluate(CurrentSolution);
+	
+	repeat= true;
+	while repeat
+		NeighbourBest= Inf;
+		for i=1:size(CurrentSolution,1)
+			NeighbourSolution= BuildNeighbour(CurrentSolution,i);
+			NeighbourObjective= Evaluate(NeighbourSolution);
+			if NeighbourObjective < NeighbourBest
+				NeighbourBest= NeighbourObjective;
+				NeighbourBestSolution= NeighbourSolution;
+			end
+		end
+		if NeighbourBest < CurrentObjective
+			CurrentObjective= NeighbourBest;
+			CurrentSolution= NeighbourBestSolution;
+		else
+			repeat= false;
+		end
+	end
+	if CurrentObjective < GlobalBest
+		GlobalBestSolution= CurrentSolution;
+		GlobalBest= CurrentObjective;
+	end
+>>>>>>> Stashed changes:guide4/optimization.m
 end
 
 
 
+<<<<<<< Updated upstream:guide4/ex_e_optimization.m
+=======
+
+>>>>>>> Stashed changes:guide4/optimization.m
